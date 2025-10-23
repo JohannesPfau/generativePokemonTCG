94abb95de3f3d7835a5c9e2ee8eee9b07c9be9a1
JohannesPfau <Johannes.Pfau@gmx.net>
2025-10-23 17:45:57 +0200

    Localhost alternative hint

--- pokemonGenerate.py
+++ pokemonGenerate.py
@@ -492,6 +492,7 @@ if __name__ == '__main__':
         "subtypes": ["Basic"]
     }
     LMStudio_server_address = 'http://95.99.38.192:8888/'
+    # LMStudio_server_address = 'http://localhost:1234/' # In case of local LMStudio server; make sure port is correct
     listAvailableModels(LMStudio_server_address)
     promptLMStudio(pokeName, pokeTCGDict, model, systemPrompt, LMStudio_server_address, use_RAG=use_RAG, num_RAG_samples=num_RAG_samples, temperature=temperature)
 
